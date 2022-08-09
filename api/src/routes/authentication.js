05a34917f7615a660befeb54598b437440a05f73
Alejandro Cardenas <ogopogo@fibertel.com.ar>
2022-08-09 13:03:56 -0300

    Rutas authentication, helper google.verify

---
+++ api/src/routes/authentication.js
@@ -0,0 +1,19 @@
+const router = require('express').Router();
+const { check } = require('express-validator')
+// controllers 
+const { login, googleSignIn } = require("../controllers/authentication.controllers");
+const { validarCampos } = require('../middlewares/validar.campos.js');
+
+router.post('/',
+    [
+        check('email', 'Email is required and must be valid').not().isEmpty().isEmail(),
+        check('password', 'Password is required').not().isEmpty(),
+        validarCampos
+    ],
+    login
+);
+router.post('/google', googleSignIn);
+router.get('/renew', validarJwt, renewToken)
+
+
+module.exports = router;
