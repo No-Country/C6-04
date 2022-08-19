c18db4ece84d5427d1d01198d523df94fd1461bd
oliveravictor <ovictoriox@gmail.com>
2022-08-19 17:15:48 -0300

    create home

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,9 @@
+import { Home } from './components/Home'
+
 function App() {
   return (
-    <div className="App">
-      <h1 className="text-3xl font-bold underline bg-main-blue">Hello world!</h1>
+    <div>
+      <Home />
     </div>
   );
 }
