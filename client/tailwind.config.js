fa2b95ee04e061ed415638dfb26f21154df3d8af
Alejandro Cardenas <ogopogo@fibertel.com.ar>
2022-08-25 10:57:09 -0300

    actualizo rama frontend con errores

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -37,14 +37,8 @@ module.exports = {
     },
     extend: {},
   },
-<<<<<<< HEAD
-  plugins: [
-    require('@tailwindcss/line-clamp'),
-  ],
-=======
   daisyui: {
     themes: false,
   },
   plugins: [require("daisyui")],
->>>>>>> bd776c74b4161b3b4241db498131e19f8efa1e53
 };
